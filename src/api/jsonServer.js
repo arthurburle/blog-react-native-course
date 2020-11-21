45222a04a5f8ebf041b80b0746feb8ed0feff693
Arthur Burle <arthurburle@gmail.com>
2020-11-21 03:11:26 -0300

    first commit

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://57005087e814.ngrok.io",
+});
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,80 @@
+import React, { useContext, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+import { Context } from "../context/BlogContext";
+
+const IndexScreen = ({ navigation }) => {
+  const { state, addBlogPost, deleteBlogPost, getBlogPosts } = useContext(Context);
+  
+  useEffect(() => {
+    getBlogPosts();
+
+    const listener = navigation.addListener('didFocus', () => {
+      getBlogPosts();
+    });
+
+    // cancela o addListener pra se no futuro a gente tiver algum motivo pra realmente desmontar (não apenas tirar o foco da tela) essa tela em algum moneto
+    return () => {
+      listener.remove();
+    };
+  }, []);
+
+  return (
+    <View>
+      <FlatList
+        data={state}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Show", { id: item.id })}
+            >
+              <View style={styles.row}>
+                <Text style={styles.title}>{item.title}</Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather style={styles.icon} name="trash" />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+        <Feather name="plus" size={30} />
+      </TouchableOpacity>
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "gray",
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+});
+
+export default IndexScreen;
